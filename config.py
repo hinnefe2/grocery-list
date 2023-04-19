8e8c73b2270c9f2c3ea9be59431a7442b07d3fa6
Henry Hinnefeld <henry.hinnefeld@picnichealth.com>
2023-04-19 12:41:35 +0700

    Add config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+LABEL2ID = {
+    "produce": 0,
+    "meat/seafood/deli": 1,
+    "dairy": 2,
+    "canned": 3,
+    "dry goods/baking": 4,
+    "oil/sauce/condiment": 5,
+    "spices": 6,
+    "frozen": 7,
+    "alcohol": 8,
+    "other": 9,
+}
+ID2LABEL = {value: key for key, value in LABEL2ID.items()}
